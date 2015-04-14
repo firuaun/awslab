557b14754f1b87603d261b1396514b45ced2445c
firuaun <firuaun@gmail.com>
2015-04-14 11:43:34 +0200

    awslab5 initial commit

--- actions/createform.js
+++ actions/createform.js
@@ -2,11 +2,14 @@ var util = require("util");
 var helpers = require("../helpers");
 var Policy = require("../s3post").Policy;
 var S3Form = require("../s3post").S3Form;
+var waterfall = require('async-waterfall');
 var AWS_CONFIG_FILE = "config.json";
 var POLICY_FILE = "policy.json";
 var INDEX_TEMPLATE = "index.ejs";
 var AWS = require("aws-sdk");
 AWS.config.loadFromPath(AWS_CONFIG_FILE);
+
+var DOMAIN_NAME = "RafalGrzybDomain";
 
 var task = function(request, callback){
     
@@ -33,17 +36,65 @@ var task = function(request, callback){
         
 	var success;
 	if(request.query.key) {
-		var s3 = new AWS.S3();
-                var objParams = {Bucket: request.query.bucket, Key: request.query.key};
-                s3.getObject(objParams,function(err, data){
-                    return prepareSendForm({
-                            filename: request.query.key,
-                            etag: request.query.etag,
-                            bucket: request.query.bucket,
-                            url: s3.getSignedUrl('getObject', objParams),
-                            hash: helpers.calculateDigest("md5",data.Body,'hex'),
-                            metadata: data.Metadata
+            // load SimpleDB service
+            var sDB = new AWS.SimpleDB();
+            // load S3 service
+            var s3 = new AWS.S3();
+        
+                waterfall([
+                    function(call){
+                        // Bucket config
+                        var objParams = {Bucket: request.query.bucket, Key: request.query.key};
+                        console.log("1 step");
+                        s3.getObject(objParams,function(err, data){
+                            call(null,{
+                                    filename: request.query.key,
+                                    etag: request.query.etag,
+                                    bucket: request.query.bucket,
+                                    url: s3.getSignedUrl('getObject', objParams),
+                                    hash: helpers.calculateDigest("md5",data.Body,'hex'),
+                                    metadata: data.Metadata
+                                });
                         });
+                    },
+                    function(success,call){
+                        var domainParam = {
+                            DomainName: DOMAIN_NAME
+                        };
+                        sDB.domainMetadata(domainParam,
+                        function(err,domainMeta){
+                            if(err) {
+                             //domain doesnt exist
+                             sDB.createDomain(domainParam,function(err,domain){
+                                 console.log("DOMENA:",domain);
+                                 call(null,success,domain);
+                             });
+                            }
+                            else
+                                call(null,success,domainMeta);
+                        });
+                    },
+                    function(success,domain,call){
+                        sDB.putAttributes({
+                            DomainName: DOMAIN_NAME,
+                            ItemName: success.filename,
+                            Attributes:[{
+                                    Name: 'Hash',
+                                    Value: success.hash,
+                                    Replace: true
+                            },{
+                                Name: 'Bucket',
+                                Value: success.bucket,
+                                Replace: true
+                            }]
+                        },function(err,data){
+                            if(err) console.log(err);
+                            console.log("ITEM:",data);
+                            call(null,success,domain,data);
+                        });
+                    }
+                ],function(err,success,domain,item){
+                    return prepareSendForm(success,domain,item);
                 });
 	}
         else {
